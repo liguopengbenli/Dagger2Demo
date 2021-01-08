f16d2ae45bf8ecf45f504f7bf5e4963cb2b6f970
guopeng li <guopeng.li@technicolor.com>
2021-01-08 11:33:14 +0100

    Demonstration of the case dependency injection for third party lib

--- app/src/main/java/com/androidtutz/anushka/didemo/MemoryCard.java
+++ app/src/main/java/com/androidtutz/anushka/didemo/MemoryCard.java
@@ -2,8 +2,14 @@ package com.androidtutz.anushka.didemo;
 
 import javax.inject.Inject;
 
+/*
+* For demo lets assume we don't own this class it's a third party lib
+* so we can't use @Inject to its constructor
+* We have to create a new module class for this
+* */
+
 public class MemoryCard {
-    @Inject
+    //@Inject
     public MemoryCard() {
     }
 }
